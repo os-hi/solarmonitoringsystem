ffcf23b2e88032d38d475f00ec578edad2955d70
Jake <110341554+os-hi@users.noreply.github.com>
2024-03-17 11:32:10 +0800

    new update

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+    const props = defineProps({
+        name: String,
+        dataInfo: Number
+    })
+</script>
+
 <template>
-    <div class="w-[450px] h-[300px] bg-white rounded-3xl shadow-sm flex justify-center items-center text-center font-bold text-2xl text-[#4D3287]">
-        <slot></slot>
+    <div class="w-[450px] h-[300px] bg-white rounded-3xl p-4 shadow-sm font-bold text-2xl text-[#4D3287]">
+        <p v-if="props.name" class="font-light">{{ props.name }}</p>
+        <h1 v-if="props.dataInfo" class=" w-full h-5/6 flex justify-center items-center text-8xl"> {{ props.dataInfo }}%</h1>
+        <slot v-else>NO DATA</slot>
     </div>
 </template>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,41 @@
 <script setup lang="ts">
-    import Card from './Card.vue';
-    import BigCard from './BigCard.vue';
+    import {ref} from 'vue';
+
+    const name = ref("Temperature")
+    const dataInfo = ref(69)
+
+
 </script>
 
 <template>
+    <form action="">
+
+    </form>
     <div class="w-full h-full py-20 px-24 flex justify-center items-center flex-wrap gap-12">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <BigCard></BigCard>
-        <BigCard></BigCard>
+        <div class="w-[450px] h-[300px] bg-white rounded-3xl p-4 shadow-sm font-bold text-2xl text-[#4D3287]">
+            <p v-if="name" class="font-light">{{ name }}</p>
+            <h1 v-if="dataInfo" class=" w-full h-5/6 flex justify-center items-center text-8xl"> {{ dataInfo }}%</h1>
+            <h1 v-else class="w-full h-full flex justify-center items-center">NO DATA</h1>
+        </div>
+        <div class="w-[450px] h-[300px] bg-white rounded-3xl p-4 shadow-sm font-bold text-2xl text-[#4D3287]">
+            <p v-if="name" class="font-light">{{ name }}</p>
+            <h1 v-if="dataInfo" class=" w-full h-5/6 flex justify-center items-center text-8xl"> {{ dataInfo }}%</h1>
+            <h1 v-else class="w-full h-full flex justify-center items-center">NO DATA</h1>
+        </div>
+        <div class="w-[450px] h-[300px] bg-white rounded-3xl p-4 shadow-sm font-bold text-2xl text-[#4D3287]">
+            <p v-if="name" class="font-light">{{ name }}</p>
+            <h1 v-if="dataInfo" class=" w-full h-5/6 flex justify-center items-center text-8xl"> {{ dataInfo }}%</h1>
+            <h1 v-else class="w-full h-full flex justify-center items-center">NO DATA</h1>
+        </div>
+        <div class="w-[550px] h-[400px] bg-white rounded-3xl shadow-sm p-4 font-bold text-2xl text-[#4D3287]">
+            <p v-if="name" class="font-light">{{ name }}</p>
+            <h1 v-if="dataInfo" class=" w-full h-5/6 flex justify-center items-center text-9xl"> {{ dataInfo }}%</h1>
+            <h1 v-else class="w-full h-full flex justify-center items-center">NO DATA</h1>
+        </div>
+        <div class="w-[550px] h-[400px] bg-white rounded-3xl p-4 shadow-sm font-bold text-2xl text-[#4D3287]">
+            <p v-if="name" class="font-light">{{ name }}</p>
+            <h1 v-if="dataInfo" class=" w-full h-5/6 flex justify-center items-center text-9xl"> {{ dataInfo }}%</h1>
+            <h1 v-else class="w-full h-full flex justify-center items-center">NO DATA</h1>
+        </div>
     </div>
 </template>
