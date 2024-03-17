b5f15e339e57d36b24596d69c8ae0ecb106ab324
Jake <110341554+os-hi@users.noreply.github.com>
2024-03-17 13:18:42 +0800

    minor adjustments

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container flex flex-col w-full lg:w-4/6 items-center text-center p-5 xl:p-16 bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500">
+    <div class="flex flex-col w-full xl:w-4/6 items-center text-center p-5 xl:p-16 bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500">
         <h1 class="text-4xl lg:text-5xl xl:text-6xl text-white my-16 font-bold">Welcome to Solar Monitoring System</h1>
         <p class="hidden lg:block text-2xl text-white text-left mx-10">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -7,8 +7,3 @@
         </p>
     </div>
 </template>
-<!-- <style scoped>
-.container{
-    background-image: linear-gradient(to left top, #de91eb, #d178eb, #c15eeb, #ae43ed, #9624ef);
-}
-</style> -->
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,7 @@
   import Home from '../components/Home.vue'
 </script>
 <template>
-  <div class="flex justify-center items-center w-screen h-screen py-5 px-10 lg:py-10 xl:py-20 xl:px-52 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900">
+  <div class="flex justify-center items-center w-screen h-screen py-5 px-10 lg:py-10 2xl:py-20 2xl:px-52 bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900">
     <div class="w-full h-full flex flex-col lg:flex-row shadow-2xl">
       <Home/>
       <Login />
