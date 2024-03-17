8592efbc37de18848ef67de5efadd61130cdd3fc
Jake <110341554+os-hi@users.noreply.github.com>
2024-03-17 11:51:26 +0800

    added router view import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-  import LoginView from './views/LoginView.vue'
+  import { RouterView } from 'vue-router'
 </script>
 <template>
   <RouterView />
-  <!-- <LoginView /> -->
 </template>
